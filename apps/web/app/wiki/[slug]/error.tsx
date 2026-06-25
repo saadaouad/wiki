906e4e40e3875e4c18fe2d07e80129e998f53bf8
Saad <aouadsaad05@gmail.com>
2026-06-25 02:54:57 +0200

    add error boundaries

---
+++ apps/web/app/wiki/[slug]/error.tsx
@@ -0,0 +1,32 @@
+'use client' 
+ 
+import { useEffect } from 'react'
+ 
+const ErrorPage = ({
+  error,
+  unstable_retry,
+}: {
+  error: Error & { digest?: string }
+  unstable_retry: () => void
+}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service like Sentry, Bugsnag, etc.
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        type="button"
+        onClick={
+          () => unstable_retry()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
+
+export default ErrorPage;
